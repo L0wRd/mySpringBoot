8d10adb153cda5dedb7e2e536cdd7c8bb94524c2
nero <871889334@qq.com>
2018-06-12 22:27:30 +0800

    git整合测试

--- src/main/java/com/smart/service/UserService.java
+++ src/main/java/com/smart/service/UserService.java
@@ -26,6 +26,7 @@ public class UserService {
         this.userMapper = userMapper;
     }
 
+    //使用了spring自带的事物管理策略 若是要自定义事物管理还需在主类中添加bean
     @Transactional
     public List<User> findAll() {
         System.out.println("调用dao层方法");
